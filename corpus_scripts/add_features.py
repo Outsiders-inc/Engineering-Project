3956c98b42a8bba68dbbe1a30f2ac6a5290dccc6
OsoFlaco <shahar.osovsky@mail.huji.ac.il>
2018-05-21 00:08:56 +0300

    Add files via upload
    COMPLETE:
         * A feature that counts appearances of a word in the wikipedia Person tree (unigram).
         * A feature the checks if a word (unigram) is in the wikipedia Location tree (1 or 0)
    
    INCOMPLETE:
        * A feature that checks for bigram appearance in the wikipedia Location tree. Still a work in progress.

--- corpus_scripts/add_features.py
+++ corpus_scripts/add_features.py
@@ -1,11 +1,11 @@
-
 CORP_NAME = "organized_corp"
 CURRENT_CORP = CORP_NAME + ".txt"
 NEW_NAME_SUF = "_new_feature.txt"
 NEW_LINE = "\n"
 NEW_SPOT = 2
-SEP = "\t"
+SEP = "\t\t"
 
+PUNCT_OR_WORD = 2
 
 # Push to corpus with corpus_file_name a feature with values feature_values before tag.
 # Saves as a new file with the name:
@@ -58,3 +58,128 @@ def is_end_feat(corpus_file_name):
         if line == "\n":
             is_end[index - 2] = "1"
     add_feature(corpus_file_name, corpus_file_name, is_end)
+
+
+# Dictionary feature - number of occurrences as a name in Wikipedia's person list
+def appearances_wikipedia_person_feature(corpus_file_name, wiki_persons_file_name):
+    with open(corpus_file_name, "r", encoding="utf-8") as corp:
+        lines = corp.readlines()
+    words = list(map(lambda s: s.split("\t")[0], lines))
+
+    # for word in words: # TODO Delete
+    #     print(word) # TODO Delete
+
+    with open(wiki_persons_file_name, "r", encoding="utf-8") as wiki:
+        wikiLines = wiki.readlines()
+    wikiLinesStripped = list(map(lambda s: s.strip("\n").strip("\t"), wikiLines))
+    wikiWordsList = []
+    for line in wikiLinesStripped:
+        wikiWordsList += line.split(" ")
+    # for word in wikiWordsList: # TODO Delete
+    #     print(word) # TODO Delete
+
+    occurrences = ["0"] * len(lines)
+    for index, word in enumerate(words):
+        counter = 0
+        for wiki_word in wikiWordsList:
+            if word == wiki_word:
+                counter += 1
+        occurrences[index] = str(counter)
+        # print(word + " - " + str(counter)) # TODO Delete
+
+    add_feature(corpus_file_name, corpus_file_name, occurrences)
+
+
+# Dictionary feature - is name in Wikipedia's person list (as unigram)
+def is_wikipedia_location_unigram_feature(corpus_file_name, wiki_location_file_name):
+    with open(corpus_file_name, "r", encoding="utf-8") as corp:
+        lines = corp.readlines()
+    words = list(map(lambda s: s.split("\t")[0], lines))
+
+    with open(wiki_location_file_name, "r", encoding="utf-8") as wiki:
+        wikiLines = wiki.readlines()
+    wikiLinesStripped = list(map(lambda s: s.strip("\n").strip("\t"), wikiLines))
+    wikiWordsList = []
+    for line in wikiLinesStripped:
+        wikiWordsList += line.split(" ")
+
+    is_wiki_loc = ["0"] * len(lines)
+    for index, word in enumerate(words):
+        counter = 0
+        for wiki_word in wikiWordsList:
+            if word == wiki_word:
+                is_wiki_loc[index] = "1"
+                break
+
+    add_feature(corpus_file_name, corpus_file_name, is_wiki_loc)
+
+
+# Dictionary feature - is word and next word in Wikipedia's person list (as Bigram)
+def is_wikipedia_location_bigram_feature(corpus_file_name, wiki_location_file_name):
+    with open(corpus_file_name, "r", encoding="utf-8") as corp:
+        lines = corp.readlines()
+    # Creating a list of lists where each element is [word, PUNCT_OR_WORD]:
+    for i,line in enumerate(lines):
+        if line == "\n":
+            print("--empty line ---")  # TODO - DELETE
+            lines[i] = "EMPTY_LINE" + SEP + "_" + SEP + "P"
+        else:
+            # print (line)  # TODO - DELETE
+            pass
+    # for line in lines: # TODO - DELETE
+    #     line = line.split(SEP)    # TODO - DELETE
+    #     print("0 -> " + line[0] + " 1 -> " + line[1]+ " 2 -> " + line[2])     # TODO - DELETE
+    words = list(map(lambda s: s.split(SEP)[0 : (PUNCT_OR_WORD + 1) : PUNCT_OR_WORD], lines))
+
+    print("words:") # TODO - DELETE
+    print (words)   # TODO - DELETE
+
+    with open(wiki_location_file_name, "r", encoding="utf-8") as wiki:
+        wikiLines = wiki.readlines()
+    wikiLinesStripped = list(map(lambda s: s.strip("\n").strip("\t"), wikiLines))
+    # Creating a list of lists, in which for each expression - we save tuples of all consecutive words:
+    wikiWordsList = []
+    for line in wikiLinesStripped:
+        temp = line.split(" ")
+        wikiWordsList += ([temp[i], temp[i + 1]] for i in range(len(temp) - 1))
+
+    print("wikiWordsList:")  # TODO - DELETE
+    print(wikiWordsList)  # TODO - DELETE
+
+    is_wiki_loc = ["0"] * len(lines)
+    for index, word in enumerate(words):
+        counter = 0
+        print(word)   # TODO - DELETE
+        if word[1] == "P":
+            continue
+        word_tuple = word[0]
+        i = 1
+        second_word = ""
+        while words[index + i][1] == "P":
+            if words[index + i][0] == "EMPTY_LINE":
+                i = -1
+                break
+            i += 1
+        if i != -1:
+            second_word = words[index + i][0]
+            if second_word != "":
+                word_tuple += " " + second_word
+                print(word_tuple)  # TODO - DELETE
+
+                for wiki_word in wikiWordsList:
+                    if word_tuple == wiki_word:
+                        is_wiki_loc[index] = "1"
+                        print(word_tuple + " - 1")  # TODO - DELETE
+                        break
+
+
+if __name__ == '__main__':
+
+    # # Adding a wiki-person feature
+    # appearances_wikipedia_person_feature("organized_corp_test.txt", "wikiTreePerson_filtered.txt")
+
+    # # Adding a wiki-location Unigram feature
+    # is_wikipedia_location_unigram_feature("organized_corp_test.txt", "wikiTreeConstructions_filtered.txt")
+
+    # Adding a wiki-location Bigram feature
+    is_wikipedia_location_bigram_feature("‏‏organized_corp_test_small.txt", "‏‏wikiTreeConstructions_test_small.txt") # TODO - UNFINISHED FUNCTION!!!!
